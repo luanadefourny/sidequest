002698d288f820ef8f3b954f4b77e16e1720635a
luanadefourny <luanadefourny@gmail.com>
2025-08-10 18:09:47 +0100

    added proposal, .env, changed access in db.ts to env variables

--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
-const DB_PORT: string = '27017';
-const DB_NAME: string = 'sidequest';
+const DB_PORT: string | undefined = process.env.DB_PORT;
+const DB_NAME: string | undefined = process.env.DB_NAME;
 
 async function main(): Promise<void> {
   try {
