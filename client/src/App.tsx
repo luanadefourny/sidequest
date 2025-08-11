bdf447e1bf32ae2e5fdb0baec8c556f1fd423e25
Theo <theo.creweread@gmail.com>
2025-08-11 17:11:46 +0100

    app.jsx fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,17 +4,20 @@ import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import ProfilePage from './components/ProfilePage/ProfilePage';
 import FavouritesPage from './components/FavouritesPage/FavouritesPage';
+import type { User } from './types';
 import './App.css';
 import MapComponent from './components/MapComponent/MapComponent';
 
 export default function App() {
+  const user: User | null = null;
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={<ProfilePage user={user} />} />
         <Route path="/favourites" element={<FavouritesPage />} />
         <Route path="/map" element={<MapComponent />} />
       </Routes>
