f7e20ec9748429a18defa8406eeed881a86e9b1c
Theo <theo.creweread@gmail.com>
2025-08-11 09:22:42 +0100

    basic navbar and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,21 @@
-import { Routes, Route, BrowserRouter } from 'react-router';
-import TestComponent from './components/TestComponent/TestComponent.tsx'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+import ProfilePage from './components/ProfilePage/ProfilePage';
+import FavouritesPage from './components/FavouritesPage/FavouritesPage';
 import './App.css';
 
 export default function App() {
-
   return (
     <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<TestComponent />}></Route>
-      {/*<Route path="/home" element={<component here />}> </Route>*/}
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
---
+++ client/src/components/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,11 @@
+
+
+export default function RegisterPage() {
+
+  return (
+    <div className="register-page">
+    <h1>Register Page</h1>
+    <p>Welcome to the registration page!</p>
+  </div>
+  );
+}
