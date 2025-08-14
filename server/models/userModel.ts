b57f8b9298e923e01acd8ccfaca259594705ec61
luanadefourny <luanadefourny@gmail.com>
2025-08-14 16:04:03 +0100

    fixed the password situation without compromising on the model not letting it out

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -91,7 +91,7 @@ const UserSchema = new mongoose.Schema<IUser>({
   password: {
     type: String,
     required: true,
-    select: false, //cannot be grabbed to return to the user
+    select: false,
   },
   firstName: {
     type: String,
