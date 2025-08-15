a00661a8f9e049a8d6d9ff422b789b8304e67c8b
luanadefourny <luanadefourny@gmail.com>
2025-08-15 11:58:13 +0100

    mid edit change; but gotta go checkout mihais changes

--- client/src/components/ProfilePage/ProfilePage.tsx
+++ client/src/components/ProfilePage/ProfilePage.tsx
@@ -1,19 +1,32 @@
 import { useUser } from '../Context/userContext';
+import { Navigate } from 'react-router-dom';
 
 
 export default function ProfilePage() {
-  const { user } = useUser();
-  //! this fixed the rendering
+  const { user, setUser } = useUser();
+
+  if (!user) return <Navigate to="/" replace />
+  
   const birthDate = user && (typeof user.birthday === 'string' ? new Date(user.birthday) : user.birthday);
+
+  // const [editing, setEditing] = useState<boolean>(false);
+  // const [firstName, setFirstName] = useState<string>(user.firstName ?? '');
+  // const [lastName, setLastName] = useState<string>(user.lastName ?? '');
+  // const [birthday, setBirthday] = useState<Date | string>(birthDate ?? ''); // yyyy-mm-dd
+  // const [profilePicture, setProfilePicture] = useState('');
+  // const [saving, setSaving] = useState(false);
+  // const [message, setMessage] = useState<string | null>(null);
+
+  //! this fixed the rendering
   console.log(user?.profilePicture);
   
-  if (!user) {
-    return (
-      <div className="flex justify-center items-center h-screen text-gray-500">
-        Loading user data...
-      </div>
-    );
-  }
+  // if (!user) {
+  //   return (
+  //     <div className="flex justify-center items-center h-screen text-gray-500">
+  //       Loading user data...
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="max-w-3xl mx-auto p-6 bg-white shadow-md rounded-lg mt-10">
